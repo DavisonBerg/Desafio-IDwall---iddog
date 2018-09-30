52c194400bd5cc8bdb95c26d58703fa85775e34b
Davison <dbsd@bepid.cin.ufpe.br>
2018-09-30 10:30:50 -0300

    Criando o projeto

---
+++ Desafio IDwall-iddog/Desafio IDwall-iddog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desafio IDwall-iddog
+//
+//  Created by Davison Dantas on 30/09/2018.
+//  Copyright © 2018 Davison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

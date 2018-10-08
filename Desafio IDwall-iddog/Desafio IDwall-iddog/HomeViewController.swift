8b0cfa8c1f513b0a470c8c89856c2123410251d1
Davison <dbsd@bepid.cin.ufpe.br>
2018-10-08 01:35:06 -0300

    Animações

--- Desafio IDwall-iddog/Desafio IDwall-iddog/HomeViewController.swift
+++ Desafio IDwall-iddog/Desafio IDwall-iddog/HomeViewController.swift
@@ -7,10 +7,8 @@
 //
 
 import UIKit
-import ObjectMapper
 import Foundation
-import Alamofire
-import PromiseKit
+
 
 struct DogStruct : Decodable{
     let category : String
@@ -56,16 +54,20 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
         return self.ltDogs.count
     }
     
+    
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! HomeCollectionViewCell
-        
+        cell.dogImage.isUserInteractionEnabled = true
+        cell.dogImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleZoomTap)))
+        //cell.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleZoomTap)))
         cell.dogBreedLabel.text = self.ltDogs[indexPath.row].category
         let poster_path = self.ltDogs[indexPath.row].list[0]
         print(poster_path)
-                let dogImageURL = URL(string: poster_path)
-                if let dataImage = NSData(contentsOf: dogImageURL!){
-                    cell.dogImage.image = UIImage(data: dataImage as Data)
-                }
+        let dogImageURL = URL(string: poster_path)
+        if let dataImage = NSData(contentsOf: dogImageURL!){
+            cell.dogImage.image = UIImage(data: dataImage as Data)
+        }
         
         return cell
     }
@@ -92,14 +94,48 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
             }
         }.resume()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    var startingFrame: CGRect?
+    var blackBackgroundView: UIView?
+    
+    @objc func handleZoomTap(tapGesture: UITapGestureRecognizer){
+        if let imageView = tapGesture.view as? UIImageView{
+            startingFrame = imageView.superview?.convert(imageView.frame, to: nil)
+            
+            let zoomingImageView = UIImageView(frame: startingFrame!)
+            zoomingImageView.image = imageView.image
+            zoomingImageView.isUserInteractionEnabled = true
+            zoomingImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleZoomOut)))
+            
+            if let keyWindow = UIApplication.shared.keyWindow{
+                blackBackgroundView = UIView(frame: keyWindow.frame)
+                blackBackgroundView?.backgroundColor = UIColor.black
+                blackBackgroundView?.alpha = 0
+                keyWindow.addSubview(blackBackgroundView!)
+                
+                keyWindow.addSubview(zoomingImageView)
+                //Animate zoom
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.blackBackgroundView?.alpha = 1
+                    
+                    let height = self.startingFrame!.height / self.startingFrame!.width * keyWindow.frame.width
+                    
+                    zoomingImageView.frame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
+                    zoomingImageView.center = keyWindow.center
+                }, completion: nil)
+            }
+        }
     }
-    */
-
+    
+    @objc func handleZoomOut(tapGesture: UITapGestureRecognizer){
+        if let zoomOutImageView = tapGesture.view{
+            //Animate back out to controller
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                zoomOutImageView.frame = self.startingFrame!
+                self.blackBackgroundView?.alpha = 0
+            }) { (completed: Bool) in
+                zoomOutImageView.removeFromSuperview()
+            }
+        }
+    }
 }
